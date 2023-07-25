cc2cd5fa2a0e7ab3454b8a383fde42c38aacc3af
Angad Bhatti <angad@Angads-MacBook-Pro.local>
2023-07-25 14:05:57 -0700

    created inventory models and migrated them

--- h2InventoryMgmt/inventory/models.py
+++ h2InventoryMgmt/inventory/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+class Beverages(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class SnackOnTheGo(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Candy(models.Model):
+    snack_on_the_go = models.ForeignKey(SnackOnTheGo, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Snacks(models.Model):
+    snack_on_the_go = models.ForeignKey(SnackOnTheGo, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class WarmFood(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Sundries(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class FrozenItems(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
