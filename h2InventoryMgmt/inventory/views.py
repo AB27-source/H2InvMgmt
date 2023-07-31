59893376d9365021156711f71d068faaafedde2e
Angad Bhatti <angad@Angads-MacBook-Pro.local>
2023-07-31 16:25:20 -0700

    added templates

--- h2InventoryMgmt/inventory/views.py
+++ h2InventoryMgmt/inventory/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from .models import Beverage
 
 # Create your views here.
+
 def index(request):
-    return HttpResponse("You're at the inventory index.")
+    beverage_list = Beverage.objects.order_by('name')  # Order by the 'name' field
+    context = {
+        "beverage_list": beverage_list
+    }
+
+    return render(request, "inventory/index.html", context)
+
+def detail(request, beverage_id):
+    beverage = get_object_or_404(Beverage, pk=beverage_id)
+    return render(request, "polls/detail.html", {"beverage": beverage})
